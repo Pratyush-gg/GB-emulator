072e78acc721a5ac30d518379eec004ef7cf055d
Shawn Moses <shawnmoses1847@gmail.com>
2025-09-24 12:34:07 +0530

    cleaned up build files

--- build/CMakeFiles/gb-emulator.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for gb-emulator.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/gb-emulator.dir/src/ui.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/gb-emulator.dir/src/ui.cpp.o: \
- /mnt/c/Pratyush/study/GB-emulator/src/ui.cpp /usr/include/stdc-predef.h
